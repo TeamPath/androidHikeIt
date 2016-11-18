5edf15b6b3cdef63abc5d4ab19b30b16d48bab73
JeremiahLewis0710 <jeremiahlewis@JeremiahLewisMBP.local>
2016-11-18 17:00:29 -0500

    StopHike View, Layout, and Stage
    Added Views, Stages, Layout

---
+++ HikeItV22/app/src/main/java/com/pathteam/hikeitv2/Views/StopHikeView.java
@@ -0,0 +1,17 @@
+package com.pathteam.hikeitv2.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by jeremiahlewis on 11/18/16.
+ */
+
+public class StopHikeView extends RelativeLayout {
+    public StopHikeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+    }
+}
