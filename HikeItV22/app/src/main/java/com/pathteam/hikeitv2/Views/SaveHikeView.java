8129875fc114e8db3d3372eb00d4e448a5b1e13e
Joshua Mabry <inkandcapture@gmail.com>
2016-11-21 09:17:46 -0500

    Removed Action Bar

--- HikeItV22/app/src/main/java/com/pathteam/hikeitv2/Views/SaveHikeView.java
+++ HikeItV22/app/src/main/java/com/pathteam/hikeitv2/Views/SaveHikeView.java
@@ -8,10 +8,8 @@ import android.widget.RelativeLayout;
  * Created by jeremiahlewis on 11/18/16.
  */
 
-public class StopHikeView extends RelativeLayout {
-    public StopHikeView(Context context, AttributeSet attrs) {
+public class SaveHikeView extends RelativeLayout {
+    public SaveHikeView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-
     }
 }
